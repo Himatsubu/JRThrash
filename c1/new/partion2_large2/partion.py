da0ff162c688a26570eebc349c3e4a8c6878b4e3
yanai <keitaro.871@gmail.com>
2016-09-12 14:46:36 +0900

    コードの最適化

---
+++ c1/new/partion2_large2/partion.py
@@ -0,0 +1,7 @@
+print "for(j=1;j<=65;j++){"
+for i in xrange(1,130):
+    print "	sub00.u["+str(i*129)+"+k]=u["+str(129*i)+"+k];"
+print "}"
+print "for(k=64;k<=129;k++){"
+    print "	sub01.u["str(i*129)+"+k]=u["+str(129*i)+"+k];"
+print "}"
