ba3c1402a8384df3ba81f19debe3ad35344c510b
yanai <keitaro.871@gmail.com>
2016-10-11 14:25:36 +0900

    drop.shの追加

---
+++ c1/ver3/partion8/drop.sh
@@ -0,0 +1,10 @@
+sudo java -jar /home/yanai/JRThrash/JRThrash.jar *.java
+cp /home/yanai/JRThrash/result/sub00.v    /home/yanai/Dropbox/a
+cp /home/yanai/JRThrash/result/sub01.v    /home/yanai/Dropbox/a
+cp /home/yanai/JRThrash/result/sub02.v    /home/yanai/Dropbox/a
+cp /home/yanai/JRThrash/result/sub03.v    /home/yanai/Dropbox/a
+cp /home/yanai/JRThrash/result/sub04.v    /home/yanai/Dropbox/a
+cp /home/yanai/JRThrash/result/sub05.v    /home/yanai/Dropbox/a
+cp /home/yanai/JRThrash/result/sub06.v    /home/yanai/Dropbox/a
+cp /home/yanai/JRThrash/result/sub07.v    /home/yanai/Dropbox/a
+cp /home/yanai/JRThrash/result/c1.v /home/yanai/Dropbox/a
